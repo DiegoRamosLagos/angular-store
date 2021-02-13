86d055b8a619218cca01da710098605d94c924db
randir <diegoa.ramosl@gmail.com>
2021-02-13 13:11:33 -0300

    Add product-detail component & products service. Update modules and products component.

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Product } from '../product.model';
 
-@Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss'],
+@Injectable({
+  providedIn: 'root',
 })
-export class ProductsComponent implements OnInit {
+export class ProductsService {
   products: Product[] = [
     {
       id: '1',
@@ -50,11 +49,14 @@ export class ProductsComponent implements OnInit {
       description: 'bla bla bla bla bla',
     },
   ];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  getAllProducts(): any {
+    return this.products;
+  }
 
-  clickProduct(id: number): void {
-    console.log('product id', id);
+  getProduct(id: string): any {
+    return this.products.find((item) => id === item.id);
   }
 }
