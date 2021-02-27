02cf628fc0e498babe722bb0a8c9a07bf6ba96bc
Diego Ramos <diegoa.ramosl@gmail.com>
2021-02-27 01:50:55 -0300

    Revert "Forms"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,82 +1,10 @@
 import { NgModule } from '@angular/core';
-import {
-  Routes,
-  RouterModule,
-  PreloadAllModules,
-} from '@angular/router';
-import { AdminGuard } from './admin.guard';
-import { DemoComponent } from './demo/demo.component';
-import { LayoutComponent } from './layout/components/layout/layout.component';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: LayoutComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: '/home',
-        pathMatch: 'full',
-      },
-      {
-        path: 'home',
-        loadChildren: () =>
-          import('./home/home.module').then(
-            (module) => module.HomeModule,
-          ),
-      },
-      {
-        path: 'products',
-        loadChildren: () =>
-          import('./product/product.module').then(
-            (module) => module.ProductModule,
-          ),
-        canActivate: [AdminGuard],
-      },
-      {
-        path: 'contact',
-        loadChildren: () =>
-          import('./contact/contact.module').then(
-            (module) => module.ContactModule,
-          ),
-        canActivate: [AdminGuard],
-      },
-    ],
-  },
-  {
-    path: 'demo',
-    component: DemoComponent,
-    canActivate: [AdminGuard],
-  },
-  {
-    path: 'admin',
-    loadChildren: () =>
-      import('./admin/admin.module').then(
-        (module) => module.AdminModule,
-      ),
-  },
-  {
-    path: 'auth',
-    loadChildren: () =>
-      import('./auth/auth.module').then(
-        (module) => module.AuthModule,
-      ),
-  },
-  {
-    path: '**',
-    loadChildren: () =>
-      import('./page-not-found/page-not-found.module').then(
-        (module) => module.PageNotFoundModule,
-      ),
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules,
-    }),
-  ],
-  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
